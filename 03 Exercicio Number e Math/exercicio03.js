66010933a56e5c51a9309e635d9064f330f7290f
Moises Lopes - DRM <moiseslopes999@gmail.com>
2023-12-11 09:18:25 -0300

    teste mudar css

--- 03 Exercicio Number e Math/exercicio03.js
+++ 03 Exercicio Number e Math/exercicio03.js
@@ -10,9 +10,28 @@ let text = document.getElementsByClassName("texto")
 
 numero.innerHTML = num1
 
-function mudaBackground (text){
-    background = red;
-}
+    // ALTERAR PROPRIEDADE CSS 
+
+        const classeQueDesejoAlterar = ".texto";
+
+        /* Lista e percorre todos os Stylesheet da página */
+        for (let stylesheet of document.styleSheets) {
+
+            /* Lista e filtra a regra do CSS */
+            let cssRuleList = [...document.styleSheets[0].cssRules].filter(rule => rule.selectorText == classeQueDesejoAlterar)
+
+            /* Percorre todas as regras encontradas e aplica o CSS. */
+            for (let cssRule of cssRuleList) {
+                cssRule.style.setProperty("display", "inline-block")
+                cssRule.style.setProperty("width", "300px")
+                cssRule.style.setProperty("background", "red")
+                cssRule.style.setProperty("color", "yellow")
+                cssRule.style.setProperty("padding", "30px")
+                cssRule.style.setProperty("margin-left", "600px")
+            }
+        }
+
+    // FIM ALTERAR PROPRIEDADE CSS
 
 texto.innerHTML += (`<p> A Raiz quadrada do seu Número é = ${num1 ** 0.5} </p>`)
 texto.innerHTML += (`<p> Seu número é inteiro ? = ${ Number.isInteger(num1)} </p>`)
