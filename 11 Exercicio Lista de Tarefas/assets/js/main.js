d5457e74340e60afa0e38a2c07a6a73f69d119e0
Moises Lopes <moiseslopes999@gmail.com>
2023-12-25 09:09:35 -0300

    Final Exercicio Lista Tarefas

--- 11 Exercicio Lista de Tarefas/assets/js/main.js
+++ 11 Exercicio Lista de Tarefas/assets/js/main.js
@@ -1,36 +1,97 @@
-const inputNovaTarefa = document.querySelector(".novaTarefa");
-const btnAdicionar = document.querySelector(".btnAdicionar");
-const btnExcluir = document.querySelector(".btnExcluir");
-const tarefas = document.querySelector(".tarefas");
+function MinhaListaDeTarefas() {
+  // CONSTANTES QUE CAPTURAM OS ELEMENTOS DENTRO DO HTML
+  const inputNovaTarefa = document.querySelector(".novaTarefa");
+  const btnAdicionar = document.querySelector(".btnAdicionar");
+  const tarefas = document.querySelector(".tarefas");
 
-let textoTarefa = inputNovaTarefa.value;
+  // VARIAVEL QUE VAI RECEBER OS VALORES DIGITADOS NO INPUT
+  let textoTarefa = inputNovaTarefa.value;
 
-function criaElementoTarefa() {
-  const li = document.createElement("li");
-  return li;
-}
+  // FUNÇÃO QUE CRIA UM ELEMENTO "LI"
+  function criaElementoTarefa() {
+    const li = document.createElement("li");
+    return li;
+  }
 
-function adicionaTarefa(textoTarefa) {
-  const tarefa = criaElementoTarefa();
-  tarefas.appendChild(tarefa);
-  tarefa.innerHTML = textoTarefa;
-}
+  // FUNÇÃO QUE ADICIONA O ELEMENTO "LI" CRIADO + BOTÃO DE EXCLUIR DENTRO DA UL "TAREFAS" E ESCREVE DENTRO DO "LI"
+  function adicionaTarefa(textoTarefa) {
+    const tarefa = criaElementoTarefa();
+    tarefas.appendChild(tarefa);
+    tarefa.innerHTML = `<span>${textoTarefa}</span>`;
+    criaBtnExcluir(tarefa);
+    salvarTarefa();
+  }
 
-function limpaInput() {
-  inputNovaTarefa.value = "";
-  inputNovaTarefa.focus();
-}
+  // FUNÇÃO QUE CRIA O BOTÃO DE ESCLUIR EM CADA "LI"
+  function criaBtnExcluir(tarefa) {
+    tarefa.innerHTML += " ";
+    const btnExcluir = document.createElement("button");
+    btnExcluir.classList.add("btnExcluir");
+    btnExcluir.classList.add("fa-solid");
+    btnExcluir.classList.add("fa-xmark");
+    tarefa.appendChild(btnExcluir);
+  }
 
-inputNovaTarefa.addEventListener('keypress', function (pressionaTecla) {
+  // FUNÇÃO QUE LIMPA O INPUT E COLOCA UM "FOCUS"
+  function limpaInput() {
+    inputNovaTarefa.value = "";
+    inputNovaTarefa.focus();
+  }
+
+  // CAPTURA O EVENTO DE PRESSIONAR TECLA ENTER ( KEYCODE = 13 )
+  inputNovaTarefa.addEventListener("keypress", function (pressionaTecla) {
     if (pressionaTecla.keyCode === 13) {
-        if (inputNovaTarefa.value == "") return;
-        adicionaTarefa(inputNovaTarefa.value);
-        limpaInput();
+      if (inputNovaTarefa.value == "") return;
+      adicionaTarefa(inputNovaTarefa.value);
+      limpaInput();
     }
-});
+  });
 
-btnAdicionar.addEventListener("click", function () {
-  if (inputNovaTarefa.value == "") return;
-  adicionaTarefa(inputNovaTarefa.value);
-  limpaInput();
-});
+  // CAPTURA O EVENTO DE CLICAR NO BOTÃO "BTNADICIONAR"
+  btnAdicionar.addEventListener("click", function () {
+    if (inputNovaTarefa.value == "") return;
+    adicionaTarefa(inputNovaTarefa.value);
+    limpaInput();
+  });
+
+  // CAPTURA O EVENTO DE CLICK NO DOCUMETO, BUSCANDO OS BOTÕES COM CLASSE "BTNEXCLUIR" E REMOVE OS "PARENTES" DESSE BOTÃO, QUE NO CASO SÃO OS "LI"
+  document.addEventListener("click", function (clicado) {
+    const click = clicado.target;
+
+    if (click.classList.contains("btnExcluir")) {
+      click.parentElement.remove();
+    }
+    salvarTarefa();
+    inputNovaTarefa.value = "";
+    inputNovaTarefa.focus();
+  });
+
+  // FUNÇÃO QUE PEGA O TEXTO DOS "SPANS" QUE ESTÃO DENTRO DO ELEMENTO "TAREFAS" E SALVA EM UM "ARRAY"
+  function salvarTarefa() {
+    const textoTarefas = tarefas.querySelectorAll("span");
+    const arrayTarefas = [];
+
+    for (let tarefa of textoTarefas) {
+      let textoTarefa = tarefa.innerText;
+      arrayTarefas.push(textoTarefa);
+    }
+
+    // CRIA UM JSON E SALVA NO "LOCAL STORAGE" DO NAVEGADOR
+    const tarefasJSON = JSON.stringify(arrayTarefas);
+    localStorage.setItem("MinhasTarefas", tarefasJSON);
+  }
+
+  // FUNCÇÃO QUE PEGA OS DADOS SALVOS DO JSON NO "LOCAL STORAGE" DO NAVEGADOR
+  function TarefasSalvas() {
+    const tarefas = localStorage.getItem("MinhasTarefas");
+    const arrayTarefas = JSON.parse(tarefas);
+
+    for (let tarefa of arrayTarefas) {
+      adicionaTarefa(tarefa);
+    }
+  }
+
+  TarefasSalvas();
+}
+
+MinhaListaDeTarefas();
