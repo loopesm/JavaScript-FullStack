37ce0f8cbf3553ee02e1e6136329dece4ee7580e
Moises Lopes <moiseslopes999@gmail.com>
2024-02-24 16:31:35 -0300

    Valida CPF Classes

--- 17 Exercício CPF Validação - Classes/assets/js/index.js
+++ 17 Exercício CPF Validação - Classes/assets/js/index.js
@@ -1,30 +1,54 @@
-const falar = {
-  falar() {
-    console.log(`${this.nome} está falando.`);
-  },
-};
+// 705.484.450-52 070.987.720-03
+class ValidaCPF {
+  constructor(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+      writable: false,
+      enumerable: true,
+      configurable: false,
+      value: cpfEnviado.replace(/\D+/g, '')
+    });
+  }
 
-const comer = {
-  comer() {
-    console.log(`${this.nome} está comendo.`);
-  },
-};
+  éSequência() {
+    return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+  }
 
-const beber = {
-  beber() {
-    console.log(`${this.nome} está bebendo.`);
-  },
-};
+  geraNovoCpf() {
+    const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+    const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+    const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+    this.novoCPF = cpfSemDigitos + digito1 + digito2;
+  }
 
-const pessoaPrototype = Object.assign({}, falar, comer, beber);
+  static geraDigito(cpfSemDigitos) {
+    let total = 0;
+    let reverso = cpfSemDigitos.length + 1;
 
-function criaPessoa(nome, sobrenome) {
-  return Object.create(pessoaPrototype, {
-    nome: { value: nome },
-    sobrenome: { value: sobrenome }
-  });
+    for(let stringNumerica of cpfSemDigitos) {
+      total += reverso * Number(stringNumerica);
+      reverso--;
+    }
+
+    const digito = 11 - (total % 11);
+    return digito <= 9 ? String(digito) : '0';
+  }
+
+  valida() {
+    if(!this.cpfLimpo) return false;
+    if(typeof this.cpfLimpo !== 'string') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.éSequência()) return false;
+    this.geraNovoCpf();
+
+    return this.novoCPF === this.cpfLimpo;
+  }
 }
 
-const p1 = criaPessoa('Luiz', 'Otávio');
-const p2 = criaPessoa('Maria', 'A.');
-console.log(p2);
+let validacpf = new ValidaCPF('070.987.720-03');
+// validacpf = new ValidaCPF('999.999.999-99');
+
+if (validacpf.valida()) {
+  console.log('CPF válido');
+} else {
+  console.log('CPF inválido');
+}
