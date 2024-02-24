a10db204bc7e2fce4f3de123ca94a37210f3520d
Moises Lopes <moiseslopes999@gmail.com>
2024-02-24 19:20:00 -0300

    Valida Formulario - Classes

--- 18 Exercício Valida Formulario - Classes/assets/js/index.js
+++ 18 Exercício Valida Formulario - Classes/assets/js/index.js
@@ -1,54 +1,117 @@
-// 705.484.450-52 070.987.720-03
-class ValidaCPF {
-  constructor(cpfEnviado) {
-    Object.defineProperty(this, 'cpfLimpo', {
-      writable: false,
-      enumerable: true,
-      configurable: false,
-      value: cpfEnviado.replace(/\D+/g, '')
+// CRIAÇÃO DA CLASSE PARA VALIDAÇÃO DO FORMULARIO
+class ValidaFormulario {
+  constructor() {
+    // SELECIONAR O FORMULARIO PELA CLASSE DO FORM HTML
+    this.formulario = document.querySelector(".meuForm");
+
+    // CHAMADA DE MÉTODOS CRIADOS
+    this.eventos();
+  }
+
+  // METODO DE EVENTOS
+  eventos() {
+    // PEGAR O EVENTO DE SUBMIT DO BOTÃO
+    this.formulario.addEventListener("submit", (e) => {
+      // ARROW FUNCTION QUE CHAMA O MMETODO HANDLE SUBMIT
+      this.handleSubmit(e);
     });
   }
 
-  éSequência() {
-    return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+  // METODO QUE FAZ COM QUE O FORMULARIO NÃO SEJA ENVIADO ANTES DA VALIDAÇÃO DOS DADOS
+  handleSubmit(e) {
+    // PREVENTDEFAULT QUE FAZ O FORMULARIO NÃO SER ENVIADO
+    e.preventDefault();
+    const camposValidos = this.camposSaoValidos();
+    const senhasValidas = this.senhasSaoValidas();
+
+    if (camposValidos && senhasValidas) {
+      alert("Formulario Enviado");
+      this.formulario.submit();
+    }
   }
 
-  geraNovoCpf() {
-    const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
-    const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
-    const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
-    this.novoCPF = cpfSemDigitos + digito1 + digito2;
-  }
+  senhasSaoValidas() {
+    let valid = true;
 
-  static geraDigito(cpfSemDigitos) {
-    let total = 0;
-    let reverso = cpfSemDigitos.length + 1;
+    const senha = this.formulario.querySelector(".senha");
+    const repetirSenha = this.formulario.querySelector(".repetirSenha");
 
-    for(let stringNumerica of cpfSemDigitos) {
-      total += reverso * Number(stringNumerica);
-      reverso--;
+    if (senha.value !== repetirSenha.value) {
+      valid = false;
+      this.criaErro(senha, "Campo Senha e Repetir Senha precisam ser iguais.");
+      this.criaErro(
+        repetirSenha,
+        "Campo Senha e Repetir Senha precisam ser iguais."
+      );
     }
 
-    const digito = 11 - (total % 11);
-    return digito <= 9 ? String(digito) : '0';
+    if (senha.value.length < 6 || senha.value.length > 12) {
+      valid = false;
+      this.criaErro(senha, "Senha precisa ter entre 6 e 12 caracteres.");
+    }
+
+    return valid;
   }
 
-  valida() {
-    if(!this.cpfLimpo) return false;
-    if(typeof this.cpfLimpo !== 'string') return false;
-    if(this.cpfLimpo.length !== 11) return false;
-    if(this.éSequência()) return false;
-    this.geraNovoCpf();
+  camposSaoValidos() {
+    let valid = true;
 
-    return this.novoCPF === this.cpfLimpo;
+    for (let errorText of this.formulario.querySelectorAll(".error-text")) {
+      errorText.remove();
+    }
+
+    for (let campo of this.formulario.querySelectorAll(".validar")) {
+      const label = campo.previousElementSibling.innerText;
+
+      if (!campo.value) {
+        this.criaErro(campo, `Campo "${label}" não pode estar vazio.`);
+        valid = false;
+      }
+
+      if (campo.classList.contains("cpf")) {
+        if (!this.validaCPF(campo)) valid = false;
+      }
+
+      if (campo.classList.contains("usuario")) {
+        if (!this.validaUsuario(campo)) valid = false;
+      }
+    }
+
+    return valid;
+  }
+
+  validaUsuario(campo) {
+    const usuario = campo.value;
+    let valid = true;
+    if (usuario.length < 6 || usuario.length > 12) {
+      this.criaErro(campo, "Usuário precisa ter entre 6 e 12 caracteres");
+      valid = false;
+    }
+    if (!usuario.match(/^[a-zA-Z0-9]+$/g)) {
+      this.criaErro(campo, "Usuário precisa conter Letras ou Numeros");
+      valid = false;
+    }
+    return valid;
+  }
+
+  validaCPF(campo) {
+    const cpf = new ValidaCPF(campo.value);
+
+    if (!cpf.valida()) {
+      this.criaErro(campo, "CPF Inválido.");
+      return false;
+    }
+
+    return true;
+  }
+
+  criaErro(campo, mensagem) {
+    const div = document.createElement("div");
+    div.innerHTML = mensagem;
+    div.classList.add("error-text");
+    campo.insertAdjacentElement("afterend", div);
   }
 }
 
-let validacpf = new ValidaCPF('070.987.720-03');
-// validacpf = new ValidaCPF('999.999.999-99');
-
-if (validacpf.valida()) {
-  console.log('CPF válido');
-} else {
-  console.log('CPF inválido');
-}
+// CHAMADA DA CLASSE CRIADA
+const valida = new ValidaFormulario();
