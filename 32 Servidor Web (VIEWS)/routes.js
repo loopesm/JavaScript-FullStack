bca54664ce905d422617bad135861db1c06aa00b
Moises Lopes <moiseslopes999@gmail.com>
2024-07-09 15:30:15 -0300

    Finalizado VIEWS

--- 32 Servidor Web (VIEWS)/routes.js
+++ 32 Servidor Web (VIEWS)/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const route = express.Router()
-const homeController = require('./controllers/homeController')
+const homeController = require('./src/controllers/homeController')
 
 //Rotas HOME PAGE
 route.get('/', homeController.paginaInicial)
