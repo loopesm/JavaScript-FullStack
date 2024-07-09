8904d8269f67e7dd711ac966989f0d73c502ac4f
Moises Lopes <moiseslopes999@gmail.com>
2024-07-09 15:15:42 -0300

    Finalizando Routes e Controllers

--- 31 Servidor Web (ROUTER E CONTROLLERS)/server.js
+++ 31 Servidor Web (ROUTER E CONTROLLERS)/server.js
@@ -4,30 +4,10 @@
 
 const express = require("express");
 const app = express();
+const routes = require('./routes')
 
 app.use(express.urlencoded({extended:true}))
-
-app.get('/', (req, res) => {
-    res.send (`
-        <form action="/" method="POST">
-        Nome do Cliente: <input type="text" name="nome" >
-        <button>Enviar Formulário</button>
-        </form>
-    `)
-})
-
-app.get('/teste/:idUsuarios?', (req,res) => {
-    console.log(req.params)
-    console.log(req.query)
-    res.send(req.params.idUsuarios)
-})
-
-app.post('/', (req, res) =>{
-    console.log(req.body)
-    res.send('Recebi o Formulario')
-})
-
-
+app.use(routes)
 
 app.listen(3000, () => {
     console.log ("Acessar http://localhost:3000")
