d95bb19a4ccb5194aab648458ea67a5b27cdd4d6
Moises Lopes <moiseslopes999@gmail.com>
2024-07-09 12:58:34 -0300

    instalação do nodemon no Server

--- 29 Servidor Web (EXPRESS+NODEMON)/server.js
+++ 29 Servidor Web (EXPRESS+NODEMON)/server.js
@@ -8,8 +8,8 @@ const app = express();
 app.get('/', (req, res) => {
     res.send (`
         <form action="/" method="POST">
-        Nome: <input type="text" name="nome" >
-        <button>Enviar</button>
+        Nome do Cliente: <input type="text" name="nome" >
+        <button>Enviar Formulário</button>
         </form>
     `)
 })
