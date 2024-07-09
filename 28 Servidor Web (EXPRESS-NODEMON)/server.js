90d84434d16eff35d936c49f33d960f50e44d8b1
Moises Lopes <moiseslopes999@gmail.com>
2024-07-09 12:39:16 -0300

    Final Parte 01

--- 28 Servidor Web (EXPRESS-NODEMON)/server.js
+++ 28 Servidor Web (EXPRESS-NODEMON)/server.js
@@ -6,7 +6,19 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send ('Hello World!')
+    res.send (`
+        <form action="/" method="POST">
+        Nome: <input type="text" name="nome" >
+        <button>Enviar</button>
+        </form>
+    `)
 })
 
-app.listen(3000)
+app.post('/', (req, res) =>{
+    res.send('Recebi o Formulario')
+})
+
+app.listen(3000, () => {
+    console.log ("Acessar http://localhost:3000")
+    console.log ("Servidor executando na porta 3000")
+})
