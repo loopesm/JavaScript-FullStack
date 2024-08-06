7d3c635032949fac60d908e7e9681009e06bd770
Moises Lopes <moiseslopes999@gmail.com>
2024-08-05 23:27:55 -0300

    Inicio Projeto Agenda - NPM dependencias Up date

--- 42 Projeto Agenda ( Servidor - Express - MongoDB )/frontend/main.js
+++ 42 Projeto Agenda ( Servidor - Express - MongoDB )/frontend/main.js
@@ -1,6 +1,5 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
 import './assets/css/style.css';
-
-console.log('Olá mundo 3');
--- 42 Projeto Agenda ( Servidor - Express - MongoDB )/server.js
+++ 42 Projeto Agenda ( Servidor - Express - MongoDB )/server.js
@@ -1,53 +1,59 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    app.emit('pronto');
-  })
-  .catch(e => console.log(e));
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const flash = require('connect-flash');
-const routes = require('./routes');
-const path = require('path');
-const helmet = require('helmet');
-const csrf = require('csurf');
-const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
+//           CRIAR   LER   ATUALIZAR  APAGAR
+// CRUD  ->  CREATE  READ  UPDATE     DELETE
+//           POST    GET   PUT        DELETE
 
-app.use(helmet());
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.connectionString)
+  .then(() => {
+    app.emit("conectado");
+    console.log("Conectado com o BD");
+  })
+  .catch((e) => console.log(e));
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+
+const routes = require("./routes");
+const path = require("path");
+const meuMiddlewares = require("./src/middlewares/middlewares");
 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+// Nosso Proprio MIddlewares Global que intercepta todas as requisições
+app.use(meuMiddlewares);
+app.use(routes);
+
+app.on("conectado", () => {
+  app.listen(3000, () => {
+    console.log("Acessar http://localhost:3000");
+    console.log("Servidor executando na porta 3000");
+  });
+});
 
 const sessionOptions = session({
-  secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
-  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  secret: "chavealeatoriaSHUAHSHDAUSIDHUAISDIJNAS",
+  store: MongoStore.create({ mongoUrl: process.env.connectionString }),
   resave: false,
   saveUninitialized: false,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7,
-    httpOnly: true
-  }
+    httpOnly: true,
+  },
 });
+
 app.use(sessionOptions);
 app.use(flash());
-
-app.set('views', path.resolve(__dirname, 'src', 'views'));
-app.set('view engine', 'ejs');
-
-app.use(csrf());
-// Nossos próprios middlewares
-app.use(middlewareGlobal);
-app.use(checkCsrfError);
-app.use(csrfMiddleware);
-app.use(routes);
-
-app.on('pronto', () => {
-  app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000');
-    console.log('Servidor executando na porta 3000');
-  });
-});
