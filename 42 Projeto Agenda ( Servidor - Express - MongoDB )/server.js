2a75d171edfe9b4a8fe15be40f18c24401182688
Moises Lopes <moiseslopes999@gmail.com>
2024-09-30 23:29:20 -0300

    Aplicação com Página de Login Funcionando e CSRF Ativo e Direcionando erro para a Página 404

--- 42 Projeto Agenda ( Servidor - Express - MongoDB )/server.js
+++ 42 Projeto Agenda ( Servidor - Express - MongoDB )/server.js
@@ -19,7 +19,7 @@ const flash = require("connect-flash");
 
 const routes = require("./routes");
 const path = require("path");
-const { middlewareGlobal } = require("./src/middlewares/middleware");
+const { middlewareGlobal, checkCsrError, csrfMiddleware } = require("./src/middlewares/middleware");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -60,6 +60,8 @@ app.use(express.json());
 
 // Nossos próprios middlewares
 app.use(middlewareGlobal);
+app.use(checkCsrError)
+app.use(csrfMiddleware)
 app.use(routes);
 
 app.on("Conectado", () => {
--- 42 Projeto Agenda ( Servidor - Express - MongoDB )/src/middlewares/middleware.js
+++ 42 Projeto Agenda ( Servidor - Express - MongoDB )/src/middlewares/middleware.js
@@ -6,3 +6,15 @@ exports.middlewareGlobal = (req, res, next) => {
 exports.outroMiddleware = (req, res, next) => {
   next();
 };
+
+exports.checkCsrError = (err, req, res, next) => {
+  if(err) {
+    return res.render('404')
+  }
+  next();
+}
+
+exports.csrfMiddleware = (req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+}
